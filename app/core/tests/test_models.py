f0f5d02a1b7824ba1f87c8fdf141bf33c0fcc54f
rajpranit <rajpranit1207@gmail.com>
2022-06-25 21:21:16 +0530

    admin panel customization

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 class ModelTest(TestCase):
+
     def test_create_user_with_email_successful(self):
         email = 'test@example.com'
         password = 'example123@'
@@ -14,7 +15,7 @@ class ModelTest(TestCase):
 
 
     def test_new_user_email_normalized(self):
-            """Test email is normalized for new users."""
+            '''Test email is normalized for new users.'''
             sample_emails = [
                 ['test1@EXAMPLE.com', 'test1@example.com'],
                 ['Test2@Example.com', 'Test2@example.com'],
