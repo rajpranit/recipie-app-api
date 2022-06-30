8030384ed07269efbf4df93bd3483b571dcbed5c
rajpranit <rajpranit1207@gmail.com>
2022-06-30 19:19:58 +0530

    recipe model and create recipe app

--- app/core/models.py
+++ app/core/models.py
@@ -1,12 +1,15 @@
 """
 Database models.
 """
+from django.conf import settings
+
 from django.db import models
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
     PermissionsMixin,
 )
+
 
 class UserManager(BaseUserManager):
 
@@ -22,12 +25,12 @@ class UserManager(BaseUserManager):
 
     def create_superuser(self, email, password):
 
-            user = self.create_user(email, password)
-            user.is_staff = True
-            user.is_superuser = True
-            user.save(using=self._db)
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        user.save(using=self._db)
 
-            return user
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -40,3 +43,16 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Recipe(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    title = models.CharField(max_length=225)
+    description = models.TextField(blank=True)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+    def __str__(self):
+        return self.title
